be49c887a8b0bc60a4ef5c9d7b549b2242c3c8d8
yuvasugi339-alt <yuvasugi339@gmail.com>
2026-01-30 17:01:43 +0000

    update: description of changes

--- Hello.java
+++ Hello.java
@@ -3,6 +3,42 @@ class Hello
   {
     public static void main(String args[])
     {
-      System.out.println("Hello");
+      Scanner sc=new Scanner(System.in);
+      int m[] = new int[10];
+      int i,p=0,k=0,lb=0,ub=9;
+      System.out.println("Enter 10 numbers:");
+      for(i=0;i<10;i++)
+      {
+        m[i]=sc.nextInt();
+      }
+      System.out.println("The number to be searched:");
+      int ns=sc.nextInt();
+      while(lb<=ub)
+      {
+        p = (lb+ub)/2;
+        if(m[p]<=ns)
+        {
+          lb=p+1;
+        }
+        if(m[p]>=ns)
+        {
+          ub=p-1;
+        }
+        if(m[p]==ns)
+        {
+          k=1;
+          break;
+        }
+      }
+      if(k==1)
+      {
+        System.out.println("Number found at position:"+(p+1));
+      }
+      else
+      {
+        System.out.println("Number not found");
+      }
+      sc.close();
     }
+    
   }  
